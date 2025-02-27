033efcfecfc89a08333d7ecc1f26ee758b3e658f
Hient <hient@usf.edu>
2025-02-27 14:37:36 -0500

    Task 3 - Created Library Class"

--- cc_11.js
+++ cc_11.js
@@ -43,3 +43,40 @@ console.log(borrower1.borrowedBooks); // Expected output: ["The Great Gatsby"]
 
 borrower1.returnBook("The Great Gatsby");
 console.log(borrower1.borrowedBooks); // Expected output: []
+
+//Task 3: Creating a Library Class
+class Library {
+    constructor(){
+        this.books = [];
+        this.borrowers = [];
+    }
+    addBook(book){
+        this.books.push(book);
+    }
+    listBoks(){
+        this.books.forEach(book => console.log(book.getDetails()));
+    }
+    lendBook(borrowerId,isbn){
+        const book = this.books.find(b => b.isbn === isbn);
+        const borrower = this.borrowers.find(br => br.borrowerId === borrowerId);
+
+        if (book && book.copies > 0 && borrower) {
+            book.updateCopies(-1);
+            borrower.borrowBook(book.title);
+        } else {
+            console.log("Cannot lend book: either book is unavailable or borrower not found.");
+        }
+    }
+    returnBook(borrowerId, isbn){
+        const book = this.books.find(b => b.isbn === isbn);
+        const borrower = this.borrowers.find(br => br.borrowerId === borrowerId);
+
+        if (book && borrower && borrower.borrowedBooks.includes(book.title)) {
+            book.updateCopies(1);
+            borrower.returnBook(book.title);
+        } else {
+            console.log("Cannot return book: either book not found or borrower has not borrowed it.");
+        }
+    }
+}
+
